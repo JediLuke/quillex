9371e2aa84989d46411a74594d03dbd1a2ed06f4
JediLuke <luke.taylor@dubber.net>
2021-11-16 13:26:12 -0600

    Now rendering some nicely wrapped text!

--- lib/gui/components/notepad/notepad.ex
+++ lib/gui/components/notepad/notepad.ex
@@ -1,8 +1,9 @@
 defmodule QuillEx.Components.NotePad do
     use Scenic.Component
     require Logger
-    alias QuillEx.Components.NotePad.TextBoxMachv1
-    alias QuillEx.Components.NotePad.TextBoxMachv2
+    # alias QuillEx.Components.NotePad.TextBoxMachv1
+    # alias QuillEx.Components.NotePad.TextBoxMachv2
+    alias QuillEx.Components.NotePad.TextBoxMachv3
     alias QuillEx.Components.NotePad.TextBoxScrollable
 
 
@@ -44,24 +45,30 @@ defmodule QuillEx.Components.NotePad do
     def render(_notepad_state, first_render?: true) do
         text = "There was a band of merry Gentlemen"
         Scenic.Graph.build()
-        |> TextBoxMachv1.add_to_graph(%{
-            text: text,
-            frame: %{pin: {150, 150}, size: {200, 80}} # TODO frame
+        |> TextBoxMachv3.add_to_graph(%{
+            text: "The changes center around the fact that the NIF behind the put and clear functions breaks the immutable assumptions of the Erlang and Elixir languages. In other words, they operate directly on the backing memory of the texture instead of making a new copy and then changing it. This is for performance reasons. It also create several very hard to track down bugs.",
+            frame: %{pin: {150, 150}, size: {1200, 300}}
         })
-        |> TextBoxMachv2.add_to_graph(%{
-            text: text,
-            frame: %{pin: {150, 400}, size: {800, 300}}
-        })
+
+
+        # |> TextBoxMachv1.add_to_graph(%{
+        #     text: text,
+        #     frame: %{pin: {150, 150}, size: {200, 80}} # TODO frame
+        # })
+        # |> TextBoxMachv2.add_to_graph(%{
+        #     text: text,
+        #     frame: %{pin: {150, 400}, size: {800, 300}}
+        # })
         # Implements wrapping - for this, I just need to figure out where
         # existing wrapping is coming from, & tweak that
-        |> TextBoxMachv2.add_to_graph(%{
-            text: "The changes center around the fact that the NIF behind the put and clear functions breaks the immutable assumptions of the Erlang and Elixir languages. In other words, they operate directly on the backing memory of the texture instead of making a new copy and then changing it. This is for performance reasons. It also create several very hard to track down bugs.",
-            frame: %{pin: {150, 800}, size: {1200, 300}}
-        })
-        |> TextBoxScrollable.add_to_graph(%{
-            text: "The changes center around the fact that the NIF behind the put and clear functions breaks the immutable assumptions of the Erlang and Elixir languages. In other words, they operate directly on the backing memory of the texture instead of making a new copy and then changing it. This is for performance reasons. It also create several very hard to track down bugs.",
-            frame: %{pin: {1050, 200}, size: {300, 300}}
-        })
+        # |> TextBoxMachv2.add_to_graph(%{
+        #     text: "The changes center around the fact that the NIF behind the put and clear functions breaks the immutable assumptions of the Erlang and Elixir languages. In other words, they operate directly on the backing memory of the texture instead of making a new copy and then changing it. This is for performance reasons. It also create several very hard to track down bugs.",
+        #     frame: %{pin: {150, 800}, size: {1200, 300}}
+        # })
+        # |> TextBoxScrollable.add_to_graph(%{
+        #     text: "The changes center around the fact that the NIF behind the put and clear functions breaks the immutable assumptions of the Erlang and Elixir languages. In other words, they operate directly on the backing memory of the texture instead of making a new copy and then changing it. This is for performance reasons. It also create several very hard to track down bugs.",
+        #     frame: %{pin: {1050, 200}, size: {300, 300}}
+        # })
         # Scroll wrapping - for this, I can go ahead with existing text (which wraps),
         # but treat it as larger than another container. However, ultimately
         # I want to be able to disable the scroll-wrapping talked about above,
