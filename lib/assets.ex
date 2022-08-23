6b76aa6023e145a87ba42e5b36bd4869abf0af06
Luke T <luke.taylor@dubber.net>
2022-08-22 20:15:43 -0500

    fix font assets for fira code & commit iosevka

--- lib/assets.ex
+++ lib/assets.ex
@@ -3,9 +3,9 @@ defmodule QuillEx.Assets do
     otp_app: :quill_ex,
     alias: [
       ibm_plex_mono: "fonts/IBMPlexMono-Regular.ttf",
-      iosevka: "fonts/ttf-iosevka-etoile-15.6.3/iosevka-etoile-regular.ttf",
+      iosevka: "fonts/iosevka-etoile-regular.ttf",
       source_code_pro: "fonts/SourceCodePro-Regular.ttf",
-      fire_code: "fonts/FiraCode-Regular.ttf",
-      bitter: "fonts/Bitter-Regular.ttf",
+      fira_code: "fonts/FiraCode-Regular.ttf",
+      bitter: "fonts/Bitter-Regular.ttf"
     ]
 end
