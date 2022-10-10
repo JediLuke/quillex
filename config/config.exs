1ec381ce3588cdecb5b168bbab7bec15915519c2
Luke T <luke.taylor@dubber.net>
2022-10-10 15:04:32 -0500

    add environment specific configs

--- config/config.exs
+++ config/config.exs
@@ -8,10 +8,6 @@ config :event_bus,
   # most events & input gets processed through here
   topics: [:general]
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "prod.exs"
+config :logger, level: :info
+
+import_config "#{Mix.env()}.exs"
