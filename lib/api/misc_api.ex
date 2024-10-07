88c2767483e8751555fd051641212da5e2aa6cbc
JediLuke <1879634+JediLuke@users.noreply.github.com>
2024-10-06 19:16:50 -0500

    comment this too

--- lib/api/misc_api.ex
+++ lib/api/misc_api.ex
@@ -1,5 +1,5 @@
-defmodule QuillEx.API.Misc do
-  def makers_mark do
-    raise "here shoe makers mark"
-  end
-end
+# defmodule QuillEx.API.Misc do
+#   def makers_mark do
+#     raise "here shoe makers mark"
+#   end
+# end
