594a3f3921c81930c12da83b0e9d82400b71b1b8
JediLuke <1879634+JediLuke@users.noreply.github.com>
2024-12-05 23:10:49 -0800

    get fonts from sub directory so that it matches flamelex

--- lib/assets.ex
+++ lib/assets.ex
@@ -2,7 +2,7 @@ defmodule QuillEx.Assets do
   use Scenic.Assets.Static,
     otp_app: :quillex,
     alias: [
-      ibm_plex_mono: "fonts/IBMPlexMono-Regular.ttf",
+      ibm_plex_mono: "fonts/IBM_Plex_Mono/IBMPlexMono-Regular.ttf",
       iosevka: "fonts/iosevka-etoile-regular.ttf",
       source_code_pro: "fonts/SourceCodePro-Regular.ttf",
       fira_code: "fonts/FiraCode-Regular.ttf",
